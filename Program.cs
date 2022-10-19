eb76322492346950b54aa7820579d0f5addf75df
kent2pas <kentcejotupas@gmail.com>
2022-10-19 08:39:43 -0700

    githubminich7K.Tupas

--- Program.cs
+++ Program.cs
@@ -12,12 +12,16 @@ Console.WriteLine("_____________________________________________________________
 Console.WriteLine("Welcome to Reverse it");
 Console.WriteLine("Want to reverse your numbers type yes or no");
 repeat = Console.ReadLine();
+
 while( repeat != "no"){
 Console.WriteLine("Enter any numbers");
 number = Console.ReadLine();
+
 revnumber = string.Empty;
+
 for(int i = number.Length - 1; i>= 0; i--)
   revnumber += number[i];
+
 Console.WriteLine($"Reverse Number is {revnumber}");
 Console.WriteLine("Try again yes or no");
 repeat = Console.ReadLine();
